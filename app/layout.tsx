7de27c48cc8391d617f80e05371001d6e863026b
Anmol Deep <153999687+Anmolwalia07@users.noreply.github.com>
2025-12-28 20:56:19 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,28 +26,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-       <head>
-        {/* Google Tag Manager */}
-        <Script id="gtm" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-TGQN8GSX');
-          `}
-        </Script>
-        {/* End Google Tag Manager */}
-      </head>
+      
       <UserProvider>
         <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         cz-shortcut-listen="true"
       >
-          <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TGQN8GSX"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
         {children}
       </body>
       </UserProvider>
